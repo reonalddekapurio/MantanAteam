8309ca96d1d998e34a53917cd84170310eb441c0
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-09-08 14:50:17 +0900

    ActionButtons.tsxの修正

--- next-app/app/components/ActionButtons.tsx
+++ next-app/app/components/ActionButtons.tsx
@@ -2,20 +2,20 @@
 
 export default function ActionButtons() {
     const mainBtns = [
-        { label: "店員呼び出し", color: "bg-green-500" },
-        { label: "操作ガイド", color: "bg-orange-300" },
-        { label: "アレルギー情報", color: "bg-orange-300" },
-        { label: "お持ち帰り", color: "bg-orange-300" },
-        { label: "注文履歴", color: "bg-orange-300" },
-        { label: "注文カゴ", color: "bg-green-600" },
+        { label: "店員呼び出し", color: "bg-[#63A144]", css: "h-[146px] w-[168px]" ,link:"#"},
+        { label: "操作ガイド", color: "bg-[#FFAA49]" ,css: "h-[95px] w-[168px] p-1",link:"#"},
+        { label: "アレルギー情報", color: "bg-[#FFAA49]" ,css: "h-[95px] w-[168px] p-4",link:"#"},
+        { label: "お持ち帰り", color: "bg-[#FFAA49]"  ,css: "h-[95px] w-[168px]",link:"#"},
+        { label: "注文履歴", color: "bg-[#FFAA49]"  ,css: "h-[95px] w-[168px]",link:"#"},
+        { label: "注文カゴ", color: "bg-[#63A144]" ,css: "h-[207px] w-[168px]",link:"#"},
     ];
 
     return (
-        <div className="flex flex-col justify-between h-full p-3">
+        <div className="flex flex-col justify-between  p-3 gap-2 font-bold">
         {mainBtns.map((btn, i) => (
             <button
             key={i}
-            className={`${btn.color} text-white rounded-xl py-6 text-lg font-bold`}
+            className={`${btn.color} ${btn.css} text-white rounded-xl  text-[24px] `}
             >
             {btn.label}
             </button>
