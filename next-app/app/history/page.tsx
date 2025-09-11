7c6f325ef9ae38524ad4b8a9c8f3350a62b4f6f5
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-09-12 00:29:21 +0900

    css修正

--- next-app/app/history/page.tsx
+++ next-app/app/history/page.tsx
@@ -24,7 +24,7 @@ export default function OrdersPage() {
 
         {/* 左下インジケーター */}
 
-        <div className="absolute bottom-6 left-6">
+        <div className="absolute bottom-0 left-0">
             <Number />
         </div>
 
