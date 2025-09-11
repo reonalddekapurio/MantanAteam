fe0a7673cc91e97c614d48e3884f939d12a91904
uenoreo <yokunokatamari@ymobile.ne.jp>
2025-09-11 15:51:47 +0900

    店員呼び出しの作成

---
+++ next-app/app/components/AllergyModal.tsx
@@ -0,0 +1,8 @@
+
+export default function AllergyModal() {
+    return (
+        <div>
+            <h1>アレルギー表</h1>
+        </div>
+    );
+}
---
+++ next-app/app/components/GuideModal.tsx
@@ -0,0 +1,8 @@
+
+    export default function GuideModal() {
+        return (
+            <div>
+                <h1>操作ガイド</h1>
+            </div>
+        );
+    }
